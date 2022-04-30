1e9a158bd39beddebe4e3dc48681e73475edc1c4
Emmanuel Raphaels <87891440+Haelzz@users.noreply.github.com>
2022-04-30 08:05:11 +0100

    Create file commit

--- test.sh
+++ test.sh
@@ -1,1 +1,17 @@
-bash
+#! bin/bash
+
+function create_file ()
+{
+	echo "Enter file name to create: "
+	read $filename
+
+	if [[ -f $filename ]]
+	then
+		echo "$filename already exists"
+	else
+		touch $filename
+		echo "$filename created"
+	fi
+}
+
+create_file
